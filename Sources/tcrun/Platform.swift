cba6d94459717a8043f2753060dc71acf69d4960
Saleem Abdulrasool <compnerd@compnerd.org>
2025-08-19 11:24:34 -0700

    tcrun: structure tuples for readability
    This replaces the complex tuple types for structures to improve clarity
    and readability in the codebase.

---
+++ Sources/tcrun/Platform.swift
@@ -0,0 +1,36 @@
+// Copyright © 2025 Saleem Abdulrasool <compnerd@compnerd.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+internal import Foundation
+
+internal struct Platform {
+  public let id: String
+  public let SDKs: [URL]
+
+  public init(id: String, SDKs: [URL]) {
+    self.id = id
+    self.SDKs = SDKs
+  }
+
+  public func contains(sdk named: String) -> Bool {
+    SDKs.contains { $0.lastPathComponent == named }
+  }
+
+  public func sdk(named name: String) -> URL? {
+    SDKs.first { $0.lastPathComponent == name }
+  }
+}
+
+internal struct PlatformCollection {
+  public let root: URL
+  public let platforms: [Platform]
+
+  public init(root: URL, platforms: [Platform]) {
+    self.root = root
+    self.platforms = platforms
+  }
+
+  public func containing(sdk: String) -> [Platform] {
+    platforms.filter { $0.contains(sdk: sdk) }
+  }
+}
