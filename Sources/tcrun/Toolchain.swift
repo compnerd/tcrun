c4232b2fc3990d54b53021a6e8646c3b613acb0e
Saleem Abdulrasool <compnerd@compnerd.org>
2025-08-22 18:23:37 -0700

    tcrun: reorganise Windows utilities
    Reorganise the Windows API wrapper utilities into their own file.

--- Sources/tcrun/Toolchain.swift
+++ Sources/tcrun/Toolchain.swift
@@ -2,49 +2,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 internal import Foundation
-internal import WindowsCore
-
-internal func SearchExecutable(_ name: String, in directory: String? = nil)
-    throws -> String? {
-  func search(_ name: String, in directory: String? = nil, extension: String?) throws -> String? {
-    return try directory.withUTF16CString { wszDirectory in
-      return try name.withCString(encodedAs: UTF16.self) { wszFileName in
-        return try `extension`.withUTF16CString { wszExtension in
-          let dwLength = SearchPathW(wszDirectory, wszFileName, wszExtension, 0, nil, nil)
-          if dwLength == 0 {
-            let dwError = GetLastError()
-            if dwError == ERROR_FILE_NOT_FOUND { return nil }
-            throw WindowsError(dwError)
-          }
-
-          return try withUnsafeTemporaryAllocation(of: WCHAR.self, capacity: Int(dwLength)) {
-            guard let baseAddress = $0.baseAddress else { return nil }
-            let dwResult = SearchPathW(wszDirectory, wszFileName, wszExtension,
-                                       DWORD($0.count), baseAddress, nil)
-            guard dwResult > 0, dwResult < dwLength else { throw WindowsError() }
-            return String(decodingCString: baseAddress, as: UTF16.self)
-          }
-        }
-      }
-    }
-  }
-
-  guard name.pathExtension.isEmpty else {
-    return try search(name, in: directory, extension: nil)
-  }
-
-  if let path = try search(name, in: directory, extension: nil) {
-    return path
-  }
-
-  for `extension` in try GetEnvironmentVariable("PATHEXT")?.split(separator: ";") ?? [] {
-    if let result = try search(name, in: directory, extension: String(`extension`)) {
-      return result
-    }
-  }
-
-  return nil
-}
 
 internal struct Toolchain {
   public let identifier: String
