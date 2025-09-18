2ec1aa016a2298dea8bffd5a3f9c59fb84ba1d19
Saleem Abdulrasool <compnerd@compnerd.org>
2025-09-17 21:13:51 -0700

    tcrun: avoid unnecessary boxing overhead, simplify
    Use explicitly typed iterators for Toolchains, Platforms, and SDKs to
    avoid unnecessary boxing. Split up the enumerators across multiple files
    and calls to simplify the structure and code.

--- Sources/tcrun/Platform.swift
+++ Sources/tcrun/Platform.swift
@@ -8,10 +8,10 @@ internal struct Platform {
   public let location: URL
   public let SDKs: MemoizedSequence<SDK>
 
-  public init(location: URL, SDKs: MemoizedSequence<SDK>) {
+  internal init(location: URL, SDKs: SDKEnumerator) {
     self.identifier = location.lastPathComponent
     self.location = location
-    self.SDKs = SDKs
+    self.SDKs = MemoizedSequence(SDKs)
   }
 }
 
@@ -22,5 +22,42 @@ extension Platform {
 
   public func sdk(named identifier: String) -> SDK? {
     return SDKs.first { $0.identifier == identifier }
+  }
+}
+
+extension PlatformEnumerator {
+  internal struct Iterator: IteratorProtocol {
+    private let enumerator: FileManager.DirectoryEnumerator?
+
+    internal init(root: URL) {
+      self.enumerator =
+          FileManager.default.enumerator(at: root,
+                                         includingPropertiesForKeys: [.isDirectoryKey],
+                                         options: [.skipsHiddenFiles])
+    }
+
+    internal mutating func next() -> Platform? {
+      while let location = enumerator?.nextObject() as? URL {
+        guard location.lastPathComponent.hasSuffix(".platform"),
+            (try? location.isDirectory) ?? false else {
+          continue
+        }
+        return Platform(location: location, SDKs: SDKEnumerator(in: location))
+      }
+      return nil
+    }
+  }
+}
+
+internal struct PlatformEnumerator: Sequence {
+  private let root: URL
+
+  internal init(in DEVELOPER_DIR: URL, version: Version) {
+    self.root = DEVELOPER_DIR.appending(components: "Platforms", version.description,
+                                        directoryHint: .isDirectory)
+  }
+
+  internal func makeIterator() -> Iterator {
+    Iterator(root: root)
   }
 }
